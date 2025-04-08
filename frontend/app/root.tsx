e5c7751e0b097ccc1ba01b2b532bcb0306423ce6
sungminna <sungmin.na330@gmail.com>
2025-04-08 09:59:40 +0900

    feat: gtag

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -26,6 +26,17 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16988231160"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16988231160');
+          `
+        }} />
+        {/* <!-- End Google tag (gtag.js) --> */}
       </head>
       <body className="h-full">
         <QueryClientProvider client={queryClient}>
